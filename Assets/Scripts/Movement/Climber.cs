d36966a3ef4421afbe789052a0579dda380a7bac
WireWhiz <43615314+WireWhiz@users.noreply.github.com>
2019-12-19 11:49:48 -0500

    Added the climber script and demo scene.

---
+++ Assets/Scripts/Movement/Climber.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Climber : MonoBehaviour
+{
+    public ClimberHand RightHand;
+    public ClimberHand LeftHand;
+    public SteamVR_Action_Boolean ToggleGripButton;
+    public ConfigurableJoint ClimberHandle;
+
+    private bool Climbing;
+    private ClimberHand ActiveHand;
+
+    void Update()
+    {
+        updateHand(RightHand);
+        updateHand(LeftHand);
+        if (Climbing)
+        {
+            ClimberHandle.targetPosition =  -(ActiveHand.transform.position-transform.position);
+        }
+    }
+
+    void updateHand(ClimberHand Hand)
+    {
+        if (Climbing && Hand == ActiveHand)
+        {
+            if (ToggleGripButton.GetStateUp(Hand.Hand))
+            {
+                ClimberHandle.connectedBody = null;
+                Climbing = false;
+
+                GetComponent<Rigidbody>().useGravity = true;
+            }
+        }
+        else
+        {
+            if (ToggleGripButton.GetStateDown (Hand.Hand)||Hand.grabbing)
+            {
+                Hand.grabbing = true;
+                if (Hand.TouchedCount > 0)
+                {
+                    ActiveHand = Hand;
+                    Climbing = true;
+                    ClimberHandle.transform.position = Hand.transform.position;
+                    GetComponent<Rigidbody>().useGravity = false;
+                    ClimberHandle.connectedBody = GetComponent<Rigidbody>();
+                    Hand.grabbing = false;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/ClimberHand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+public class ClimberHand : MonoBehaviour
+{
+    public SteamVR_Input_Sources Hand;
+    public int TouchedCount;
+    public bool grabbing;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Climbable"))
+        {
+            TouchedCount++;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Climbable"))
+        {
+            TouchedCount--;
+        }
+    }
+}
